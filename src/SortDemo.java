cef03adf823b58550657310f586f13bd862205c3
canne4132 <canne4132@gmail.com>
2017-10-31 19:08:19 +0800

    update

--- src/SortDemo.java
+++ src/SortDemo.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created by shixi_anqi8 on 2017/10/26.
  */
@@ -187,16 +189,46 @@ public class SortDemo {
         numbers[i]=numbers[j];
         numbers[j]=tmp;
     }
+
+    //基数排序(LSD)
+    public static void radixSort(int[] numbers,int radix,int d){
+        //缓存数组
+        int[] tmp=new int[numbers.length];
+        //buckets用于记录待排序元素的信息
+        //buckets数组定义了max-min个桶
+        int[] buckets=new int[radix];
+        for(int i=0,rate=1;i<d;i++){
+            //重置count数组，开始统计下一个关键字
+            Arrays.fill(buckets,0);
+            //将numbers中的元素完全复制到tmp数组中
+            System.arraycopy(numbers,0,tmp,0,numbers.length);
+            //计算每个待排序数据的子关键字
+            for(int j=0;j<numbers.length;j++){
+                int subKey=(tmp[j]/rate)%radix;
+                buckets[subKey]++;
+            }
+            for(int j=1;j<radix;j++) {
+                buckets[j] = buckets[j] + buckets[j - 1];
+            }
+            //将子关键字对指定的数据进行排序
+            for(int m=numbers.length-1;m>=0;m--){
+                int subKey=(tmp[m]/rate)%radix;
+                numbers[--buckets[subKey]]=tmp[m];
+            }
+            rate *=radix;
+        }
+    }
     //测试
     public static void main(String[] args){
-        int[] numbers={12,2,21,2,13,3,32};
+        int[] numbers={12,2,212,2,13,3,32};
         //BubbleSort(numbers);
         //quick(numbers);
         //selectSort(numbers);
         //insertSort(numbers);
         //shellSort(numbers);
         //int[] arr=sort(numbers,0,numbers.length-1);
-        heapSort(numbers);
+        //heapSort(numbers);
+        radixSort(numbers,10,3);//d为元素的位的个数
         printArr(numbers);
     }
 
